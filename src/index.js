29cbb92b5d5fb3eac3edaf227d8bf2a8496a2779
Joey <gibbonsjoey95@gmail.com>
2024-03-26 21:01:24 -0500

    webpack is up and running

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('WE IN THIS BITCH, WE RUNNING SHIT!')
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: '/src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__, 'dist')
+        path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new HtmlWebpackPlugin({
