b941c161b3aa28b27c679fad52ac34dd98ab69bf
Joey <gibbonsjoey95@gmail.com>
2024-03-29 00:09:06 -0500

    got home.js to work

---
+++ src/home.js
@@ -0,0 +1,30 @@
+const home = (logo) => {
+    const homeContainer = document.createElement('div')
+
+    const logoDiv = document.createElement('div')
+    const h1 = document.createElement('h1')
+    const p = document.createElement('p')
+
+    const myLogo = new Image()
+    myLogo.src = logo
+    logoDiv.append(myLogo)
+
+    h1.textContent = 'The Pizza Project'
+
+    p.textContent = `At The Pizza Project, we take pride in offering an extraordinary dining experience that goes beyond the ordinary.
+     Situated in the heart of Dallas, our restaurant embodies the essence of hospitality, welcoming guests into a cozy and vibrant atmosphere.
+      Here, we celebrate the art of pizza-making, infusing each creation with passion and expertise. Our handcrafted pizzas are a testament to our commitment to quality,
+       featuring the finest ingredients sourced from local producers and authentic recipes passed down through generations. 
+       But it's not just about the food – it's about the sense of community and camaraderie that fills our dining room. 
+       Whether you're gathering with friends, enjoying a family dinner, or simply savoring a solo meal, 
+       every moment at The Pizza Project is an opportunity to create cherished memories.
+     Join us and discover the joy of good food, good company, and genuine hospitality.`
+
+    homeContainer.appendChild(logoDiv)
+    homeContainer.appendChild(h1)
+    homeContainer.appendChild(p)
+
+    return homeContainer
+}
+
+export {home}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
 import './styles.css';
 import logo from './logo.jpg';
+import { home } from './home.js'
 
 const component = () => {
     const content = document.querySelector('#content')
 
-    const logoDiv = document.createElement('div')
-    const h1 = document.createElement('h1')
-    const p = document.createElement('p')
-
-    const myLogo = new Image()
-    myLogo.src = logo
-    logoDiv.append(myLogo)
-
-    h1.textContent = 'The Pizza Project'
-
-    p.textContent = `At The Pizza Project, we take pride in offering an extraordinary dining experience that goes beyond the ordinary.
-     Situated in the heart of Dallas, our restaurant embodies the essence of hospitality, welcoming guests into a cozy and vibrant atmosphere.
-      Here, we celebrate the art of pizza-making, infusing each creation with passion and expertise. Our handcrafted pizzas are a testament to our commitment to quality,
-       featuring the finest ingredients sourced from local producers and authentic recipes passed down through generations. 
-       But it's not just about the food – it's about the sense of community and camaraderie that fills our dining room. 
-       Whether you're gathering with friends, enjoying a family dinner, or simply savoring a solo meal, 
-       every moment at The Pizza Project is an opportunity to create cherished memories.
-     Join us and discover the joy of good food, good company, and genuine hospitality.`
-
-    content.appendChild(logoDiv)
-    content.appendChild(h1)
-    content.appendChild(p)
+    content.appendChild(home(logo))
 
     return content
 }
 
 document.body.appendChild(component())
-
