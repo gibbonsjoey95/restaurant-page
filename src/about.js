d685d0b8c8f70f2ce166db1f5f31a8e7b6244780
Joey <gibbonsjoey95@gmail.com>
2024-03-31 00:42:01 -0500

    styled about page

--- src/about.js
+++ src/about.js
@@ -2,10 +2,14 @@ import { storeHours } from "./data"
 
 const about = () => {
     const aboutContainer = document.createElement('div')
+    aboutContainer.classList.add('about-container')
 
     const headerDiv = document.createElement('div') 
     const hoursDiv = document.createElement('div')
     const contactDiv = document.createElement('div')
+
+    hoursDiv.classList.add('hours-div')
+    contactDiv.classList.add('contact-div')
 
     const h1 = document.createElement('h1')
     const aboutTitleContainer = document.createElement('div')
@@ -19,12 +23,15 @@ const about = () => {
     headerDiv.appendChild(aboutTitleContainer)
 
     const hours = document.createElement('h2')
+    hours.classList.add('hours-title')
     hours.textContent = 'Hours'
 
-const hoursContainer = document.createElement('div')
+    const hoursContainer = document.createElement('div')
+    hoursContainer.classList.add('hours-container')
 
     storeHours.forEach((day) => {
         const dayHours = document.createElement('p')
+        dayHours.classList.add('day-hours')
 
         dayHours.textContent = `${day.day}: ${day.open}am - ${day.close}am`
 
@@ -34,6 +41,12 @@ const hoursContainer = document.createElement('div')
     hoursDiv.appendChild(hours)
     hoursDiv.appendChild(hoursContainer)
 
+    const contactContainer = document.createElement('div')
+    contactContainer.classList.add('contact-container')
+
+    const contactInfoContainer = document.createElement('div')
+    contactInfoContainer.classList.add('contact-info-container')
+
     const createElementWithText = (tag, text) => {
         const element = document.createElement(tag)
         element.textContent = text
@@ -41,7 +54,10 @@ const hoursContainer = document.createElement('div')
     }
     
     const appendElements = (parent, ...children) => {
-        children.forEach((child) => parent.appendChild(child))
+        children.forEach((child) => {
+            parent.appendChild(child)
+            child.classList.add('contact-child')
+        })
     }
 
     const contact = createElementWithText('h2', 'Contact Us')
@@ -49,7 +65,13 @@ const hoursContainer = document.createElement('div')
     const email = createElementWithText('p', "Email: mightbeafakeemail@fakeemail.com")
     const address = createElementWithText('p', "1234 Grove St, Dallas Tx")
 
-    appendElements(contactDiv, contact, phone, email, address)
+    contact.classList.add('contact-title')
+
+    // contactDiv.appendChild(contact)
+    appendElements(contactInfoContainer, phone, email, address)
+
+    contactDiv.appendChild(contact)
+    contactDiv.appendChild(contactInfoContainer)
     
     aboutContainer.appendChild(headerDiv)
     aboutContainer.appendChild(hoursDiv)
